c20e3f1df830d13ec44361b56cfb09846514860e
eugenesqr <eskurihin@gmail.com>
2020-09-07 10:58:58 +0300

    extra printing removed

--- pplog/__main__.py
+++ pplog/__main__.py
@@ -9,7 +9,6 @@ signal.signal(signal.SIGINT, lambda _, __: sys.exit(0))
 for line in sys.stdin:
     query = sys.argv[1] if len(sys.argv) > 1 else ''
     for block in pprint_blocks.get(line):
-        print('>>>', block.raw, block.parsed)
         transformed = pprint_blocks.transform(block.parsed, query)
         line = line.replace(block.raw, pprint(transformed))
     sys.stdout.write(line)
