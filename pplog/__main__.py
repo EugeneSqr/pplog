0c7b1eb99d370cd5d394069819fdd2c45508385a
eugenesqr <eskurihin@gmail.com>
2020-09-07 10:39:29 +0300

    ctrlc termination fixed

--- pplog/__main__.py
+++ pplog/__main__.py
@@ -4,8 +4,9 @@ import signal
 import pprint_blocks
 from pprinter import pprint
 
+signal.signal(signal.SIGINT, lambda _, __: sys.exit(0))
+
 for line in sys.stdin:
-    signal.signal(signal.SIGINT, lambda _, __: sys.exit(0))
     query = sys.argv[1] if len(sys.argv) > 1 else ''
     for block in pprint_blocks.get(line):
         transformed = pprint_blocks.transform(block.parsed, query)
