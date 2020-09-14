2fef150384942e078b587f8f55470059a07475d7
eugenesqr <eskurihin@gmail.com>
2020-09-14 12:00:43 +0300

    parse JSON as well

--- pplog/pprint_blocks.py
+++ pplog/pprint_blocks.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 import jq
 
@@ -13,13 +14,24 @@ def transform(data, query):
     except ValueError:
         return data
 
-def _parse(block_match):
-    block_string = block_match.group(0)
-    try:
-        parsed = eval(block_string) #pylint:disable=eval-used; this is by design
-    except Exception:               #pylint:disable=broad-except; eval can throw anything
-        parsed = None
+def _parse(match):
+    matched_string = _get_matched_string(match)
     return type('', (), {
-        'raw': block_string,
-        'parsed': parsed,
+        'raw': matched_string,
+        'parsed': _parse_json(matched_string) or _parse_python_dict(matched_string),
     })
+
+def _get_matched_string(match):
+    return match.group(0)
+
+def _parse_json(matched_string):
+    try:
+        return json.loads(matched_string)
+    except json.decoder.JSONDecodeError:
+        return None
+
+def _parse_python_dict(matched_string):
+    try:
+        return eval(matched_string) #pylint:disable=eval-used; this is by design
+    except Exception:               #pylint:disable=broad-except; eval can throw anything
+        return None
--- test/test_pprint_blocks.py
+++ test/test_pprint_blocks.py
@@ -10,15 +10,25 @@ def test_get_python_object_blocks():
     expected_raw = ["{'a':1}", "{'b':'bb'}"]
     expected_parsed = [{'a': 1}, {'b': 'bb'}]
     for i, block in enumerate(pprint_blocks.get("test {'a':1} log {'b':'bb'} entry")):
-        assert block.raw == expected_raw[i]
-        assert block.parsed == expected_parsed[i]
+        _assert_block(block, expected_raw[i], expected_parsed[i])
 
-def test_get_filetes_out_empty_objects():
+def test_get_empty_blocks_ignored():
     expected_raw = ["{'a':1}"]
     expected_parsed = [{'a': 1}]
     for i, block in enumerate(pprint_blocks.get("test {} log {'a':1} entry {b}")):
-        assert block.raw == expected_raw[i]
-        assert block.parsed == expected_parsed[i]
+        _assert_block(block, expected_raw[i], expected_parsed[i])
+
+def test_get_json_object_blocks():
+    expected_raw = ['{"a":1}']
+    expected_parsed = [{'a': 1}]
+    for i, block in enumerate(pprint_blocks.get('test {} log {"a":1} entry {b}')):
+        _assert_block(block, expected_raw[i], expected_parsed[i])
+
+def test_get_both_python_and_json_objects():
+    expected_raw = ['{"a":1}', "{'a':2}"]
+    expected_parsed = [{'a': 1}, {'a': 2}]
+    for i, block in enumerate(pprint_blocks.get('test {"a":1} log {\'a\':2} entry {b}')):
+        _assert_block(block, expected_raw[i], expected_parsed[i])
 
 def test_transform_invalid_query_returns_data_itself():
     data = {'a': 1}
@@ -27,3 +37,7 @@ def test_transform_invalid_query_returns_data_itself():
 @pytest.mark.parametrize('empty_data', ['', None, {}])
 def test_transform_empty_data_returns_none(empty_data):
     assert pprint_blocks.transform(empty_data, '.') == empty_data
+
+def _assert_block(actual, raw, parsed):
+    assert actual.raw == raw
+    assert actual.parsed == parsed
